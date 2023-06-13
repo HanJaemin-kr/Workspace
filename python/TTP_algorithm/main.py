d5575200a1ad0108311a8d86179f659e4f571d51
HanJaemin-kr <dudrn1947@naver.com>
2023-06-13 23:56:53 +0900

    json으로 저장하도록 구현

--- python/TTP_algorithm/main.py
+++ python/TTP_algorithm/main.py
@@ -45,20 +45,25 @@ def calculate_total_distance(tsp_solution, distance_matrix):
         total_distance += distance_matrix[city1][city2]
     return total_distance
 
+def numpy_int_to_int(obj):
+    if isinstance(obj, np.integer):
+        return int(obj)
+    return obj
+
 # 초기 문제 제시
 distance_matrix = [
       [0, 2, 5],
       [2, 0, 4],
       [5, 4, 0]
     ]
-item_values = [4, 6, 8]
-item_weights = [1, 2, 3]
+item_values = [1, 2, 1]
+item_weights = [1, 2, 1]
 knapsack_capacity = 3
 
 # ================ ga-param ================
-population_size = 1000
+population_size = 100
 elite_size = 0.2
-num_generations = 10
+num_generations = 50
 num_agents = 1
 # ================================================================
 
@@ -68,109 +73,110 @@ cs2sa_tsp_ls, cs2sa_kp_ls, cs2sa_time_ls, cs2sa_fitness_ls = [], [], [], []
 ma2b_tsp_ls, ma2b_kp_ls, ma2b_time_ls, ma2b_fitness_ls = [], [], [], []
 
 
-result = []
 
 
-for i in range(1, 9):
+for cnt in range (1, 11):
+    result = []
+    item_values = [1, 2, 1]
+    item_weights = [1, 2, 1]
+    for i in range(1, 12):
 
-    result.append({i: []})
-    distance_matrix = list(create_tsp_graph_matrix(i+2))
-    new_value = random.randint(1, 5)
-    item_values.append(new_value)
-    new_weight = random.randint(1, 3)
-    item_weights.append(new_weight)
-    knapsack_capacity += random.randint(0, 2)
+        result.append({i: []})
+        distance_matrix = list(create_tsp_graph_matrix(i+2))
+        new_value = random.randint(1, 5)
+        item_values.append(new_value)
+        new_weight = random.randint(1, 3)
+        item_weights.append(new_weight)
+        knapsack_capacity += random.randint(0, 2)
 
-    print('======== Round ', i,' ===========')
-    print(distance_matrix)
-    print('capacity : ',knapsack_capacity)
-    print(' value ==>',item_values)
-    print(' weight ==>',item_weights)
-    print('\n')
-    #brute-force Algorithm
-    start_time = time.time()
-    brute = BRUTE(distance_matrix, item_values, item_weights, knapsack_capacity)
-    best_tsp_genome, best_kp_genome, best_fitness = brute.solve_ttp_brute_force()
-    end_time = time.time()
-    best_kp_genome = list(best_kp_genome)
-    total_value = sum([item_values[i] for i in range(len(best_kp_genome)) if best_kp_genome[i] == 1])
-    total_weight = sum([item_weights[i] for i in range(len(best_kp_genome)) if best_kp_genome[i] == 1])
-    result[i - 1][i].append( { 'brute' : {
+        print('======== Round ', i,' ===========')
+        print(distance_matrix)
+        print('capacity : ',knapsack_capacity)
+        print(' value ==>',item_values)
+        print(' weight ==>',item_weights)
+        print('\n')
+
+        #brute-force Algorithm
+        if( i < 7 ):
+            start_time = time.time()
+            brute = BRUTE(distance_matrix, item_values, item_weights, knapsack_capacity)
+            best_tsp_genome, best_kp_genome, best_fitness = brute.solve_ttp_brute_force()
+            end_time = time.time()
+            best_kp_genome = list(best_kp_genome)
+            total_value = sum([item_values[i] for i in range(len(best_kp_genome)) if best_kp_genome[i] == 1])
+            total_weight = sum([item_weights[i] for i in range(len(best_kp_genome)) if best_kp_genome[i] == 1])
+            result[i - 1][i].append( { 'brute' : {
+                    "value": total_value,
+                    "weight": total_weight,
+                    "distance": calculate_total_distance(list(best_tsp_genome), distance_matrix),
+                    "fitness" : best_fitness,
+                    "time": end_time - start_time
+                }
+            })
+        else:
+            result[i - 1][i].append( { 'brute' : {
+                    "value": 0,
+                    "weight": 0,
+                    "distance": 0,
+                    "fitness" : 0,
+                    "time": 0
+                }
+            })
+
+        # co-ga Algorithm
+        start_time = time.time()
+        coga = CoGA(distance_matrix, item_values, item_weights, knapsack_capacity, population_size, elite_size, num_generations)
+
+        best_individual, ttp_fitness, value, distance, weight = coga.solve_ttp_problem()
+        end_time = time.time()
+        result[i - 1][i].append( { 'coga' : {
+                "value": value,
+                "weight": weight,
+                "distance": distance,
+                "fitness" : ttp_fitness,
+                "time": end_time - start_time
+            }
+        })
+
+        # cs2sa
+        start_time = time.time()
+        cs2sa = CS2SA()
+        best_tsp_solution, best_kp_solution, best_fitness, total_distance, selected_values, selected_weights = cs2sa.cs2sa_algorithm(distance_matrix, item_values, item_weights, knapsack_capacity)
+        end_time = time.time()
+
+        result[i - 1][i].append( { 'cs2sa' : {
+                "value": selected_values,
+                "weight": selected_weights,
+                "distance": calculate_total_distance(best_tsp_solution, distance_matrix),
+                "fitness" : best_fitness,
+                "time": end_time - start_time
+            }
+        })
+
+
+        # MA2B Algorithm
+        start_time = time.time()
+        ma2b = MA2B(distance_matrix, item_values, item_weights, knapsack_capacity)
+        tsp_solution, kp_solution, total_fitness = ma2b.ma2b_algorithm()
+        end_time = time.time()
+
+        total_value = sum([item_values[i] for i in range(len(kp_solution)) if kp_solution[i] == 1])
+        total_weight = sum([item_weights[i] for i in range(len(kp_solution)) if kp_solution[i] == 1])
+        result[i - 1][i].append({'ma2b': {
             "value": total_value,
             "weight": total_weight,
-            "distance": calculate_total_distance(list(best_tsp_genome), distance_matrix),
-            "fitness" : best_fitness,
+            "distance": calculate_total_distance(tsp_solution, distance_matrix),
+            "fitness": total_fitness,
             "time": end_time - start_time
         }
     })
 
-    # co-ga Algorithm
-    start_time = time.time()
-    coga = CoGA(distance_matrix, item_values, item_weights, knapsack_capacity, population_size, elite_size, num_generations)
+    file_path = "./test2/result_{}.txt".format(cnt)
+    # result 변수를 JSON 형식으로 변환
+    result_json = json.dumps(result, default=numpy_int_to_int)
 
-    best_individual, ttp_fitness, value, distance, weight = coga.solve_ttp_problem()
-    end_time = time.time()
-    result[i - 1][i].append( { 'coga' : {
-            "value": value,
-            "weight": weight,
-            "distance": distance,
-            "fitness" : ttp_fitness,
-            "time": end_time - start_time
-        }
-    })
-
-
-    # cs2sa
-    start_time = time.time()
-    cs2sa = CS2SA()
-    best_tsp_solution, best_kp_solution, best_fitness, total_distance, selected_values, selected_weights = cs2sa.cs2sa_algorithm(distance_matrix, item_values, item_weights, knapsack_capacity)
-    end_time = time.time()
-
-    result[i - 1][i].append( { 'cs2sa' : {
-            "value": selected_values,
-            "weight": selected_weights,
-            "distance": calculate_total_distance(best_tsp_solution, distance_matrix),
-            "fitness" : best_fitness,
-            "time": end_time - start_time
-        }
-    })
-
-
-    # MA2B Algorithm
-    start_time = time.time()
-    ma2b = MA2B(distance_matrix, item_values, item_weights, knapsack_capacity)
-    tsp_solution, kp_solution, total_fitness = ma2b.ma2b_algorithm()
-    end_time = time.time()
-
-    total_value = sum([item_values[i] for i in range(len(kp_solution)) if kp_solution[i] == 1])
-    total_weight = sum([item_weights[i] for i in range(len(kp_solution)) if kp_solution[i] == 1])
-
-    result[i - 1][i].append({'ma2b': {
-        "value": total_value,
-        "weight": total_weight,
-        "distance": calculate_total_distance(tsp_solution, distance_matrix),
-        "fitness": total_fitness,
-        "time": end_time - start_time
-    }
-    })
-
-
-print(print_result(result))
-print(result)
-
-# numpy int64 타입을 일반 int로 변환하는 함수
-# numpy int64 타입을 일반 int로 변환하는 함수
-def numpy_int_to_int(obj):
-    if isinstance(obj, np.integer):
-        return int(obj)
-    return obj
-
-# 결과를 저장할 파일 경로 및 파일명
-file_path = "result_3.txt"
-
-# result 변수를 JSON 형식으로 변환
-result_json = json.dumps(result, default=numpy_int_to_int)
-
-# JSON 형식의 결과를 파일에 저장
-with open(file_path, "w") as file:
-    file.write(result_json)
+    # JSON 형식의 결과를 파일에 저장
+    with open(file_path, "w") as file:
+        file.write(result_json)
+    file.close()
+    print('종료')
--- python/TTP_algorithm/view.py
+++ python/TTP_algorithm/view.py
@@ -1,109 +1,31 @@
-import numpy as np
+import json
+import pandas as pd
 import matplotlib.pyplot as plt
 
-import numpy as np
+# 파일에서 데이터 불러오기
+with open('./test/result_3.txt', 'r') as file:
+    data = json.load(file)
 
-def create_tsp_graph_matrix(size):
-    graph = np.random.randint(1, 10, size=(size, size))  # 임의의 그래프 매트릭스 생성
-    np.fill_diagonal(graph, 0)  # 대각선 요소는 0으로 설정
+# JSON 데이터를 Python 객체로 변환
+obj_data = json.loads(json.dumps(data))
 
-    # 모든 노드 간의 거리를 동일하게 만듦
-    for i in range(size):
-        for j in range(size):
-            if i != j:
-                graph[i][j] = graph[j][i]
+# DataFrame 생성
+df = pd.DataFrame(columns=['ID', 'Algorithm', 'Value', 'Weight', 'Distance', 'Fitness', 'Time'])
 
-    return graph
+# 데이터 프레임에 데이터 추가
+for item in obj_data:
+    for key, value in item.items():
+        for inner_item in value:
+            for algorithm, details in inner_item.items():
+                df = df.append({
+                    'ID': key,
+                    'Algorithm': algorithm,
+                    'Value': details['value'],
+                    'Weight': details['weight'],
+                    'Distance': details['distance'],
+                    'Fitness': details['fitness'],
+                    'Time': details['time']
+                }, ignore_index=True)
 
-# 매트릭스 크기 입력 받기
-size = int(input("매트릭스 크기를 입력하세요: "))
-
-# TSP 그래프 매트릭스 생성
-tsp_graph = create_tsp_graph_matrix(size)
-
-print("TSP 그래프 매트릭스:")
-print(tsp_graph)
-
-
-
-
-"""
-# Data for brute force algorithm
-brute_tsp_ls = [10, 20, 30, 40, 50]
-brute_kp_ls = [15, 25, 35, 45, 55]
-brute_time_ls = [0.5, 0.6, 0.7, 0.8, 0.9]
-brute_fitness_ls = [0.3, 0.4, 0.5, 0.6, 0.7]
-
-# Data for genetic algorithm (COGA)
-coga_tsp_ls = [20, 30, 40, 50, 60]
-coga_kp_ls = [25, 35, 45, 55, 65]
-coga_time_ls = [0.6, 0.7, 0.8, 0.9, 1.0]
-coga_fitness_ls = [0.4, 0.5, 0.6, 0.7, 0.8]
-
-# Data for simulated annealing algorithm (CS2SA)
-cs2sa_tsp_ls = [15, 25, 35, 45, 55]
-cs2sa_kp_ls = [20, 30, 40, 50, 60]
-cs2sa_time_ls = [0.7, 0.8, 0.9, 1.0, 1.1]
-cs2sa_fitness_ls = [0.5, 0.6, 0.7, 0.8, 0.9]
-
-# Data for multi-agent two-body algorithm (MA2B)
-ma2b_tsp_ls = [25, 35, 45, 55, 65]
-ma2b_kp_ls = [30, 40, 50, 60, 70]
-ma2b_time_ls = [0.8, 0.9, 1.0, 1.1, 1.2]
-ma2b_fitness_ls = [0.6, 0.7, 0.8, 0.9, 1.0]
-
-# Plotting the data
-x = [10, 20, 30, 40, 50]  # Assuming x-axis values
-
-# Plotting TSP results
-plt.figure(figsize=(10, 6))
-plt.plot(x, brute_tsp_ls, marker='o', label='Brute Force')
-plt.plot(x, coga_tsp_ls, marker='o', label='COGA')
-plt.plot(x, cs2sa_tsp_ls, marker='o', label='CS2SA')
-plt.plot(x, ma2b_tsp_ls, marker='o', label='MA2B')
-plt.xlabel('Input Size')
-plt.ylabel('TSP Result')
-plt.title('TSP Results Comparison')
-plt.legend()
-plt.grid(True)
-plt.show()
-
-# Plotting Knapsack results
-plt.figure(figsize=(10, 6))
-plt.plot(x, brute_kp_ls, marker='o', label='Brute Force')
-plt.plot(x, coga_kp_ls, marker='o', label='COGA')
-plt.plot(x, cs2sa_kp_ls, marker='o', label='CS2SA')
-plt.plot(x, ma2b_kp_ls, marker='o', label='MA2B')
-plt.xlabel('Input Size')
-plt.ylabel('Knapsack Result')
-plt.title('Knapsack Results Comparison')
-plt.legend()
-plt.grid(True)
-plt.show()
-
-# Plotting Execution Time
-plt.figure(figsize=(10, 6))
-plt.plot(x, brute_time_ls, marker='o', label='Brute Force')
-plt.plot(x, coga_time_ls, marker='o', label='COGA')
-plt.plot(x, cs2sa_time_ls, marker='o', label='CS2SA')
-plt.plot(x, ma2b_time_ls, marker='o', label='MA2B')
-plt.xlabel('Input Size')
-plt.ylabel('Execution Time (s)')
-plt.title('Execution Time Comparison')
-plt.legend()
-plt.grid(True)
-plt.show()
-
-# Plotting Fitness
-plt.figure(figsize=(10, 6))
-plt.plot(x, brute_fitness_ls, marker='o', label='Brute Force')
-plt.plot(x, coga_fitness_ls, marker='o', label='COGA')
-plt.plot(x, cs2sa_fitness_ls, marker='o', label='CS2SA')
-plt.plot(x, ma2b_fitness_ls, marker='o', label='MA2B')
-plt.xlabel('Input Size')
-plt.ylabel('Fitness')
-plt.title('Fitness Comparison')
-plt.legend()
-plt.grid(True)
-plt.show()
-"""
+# DataFrame 출력
+print(df)
